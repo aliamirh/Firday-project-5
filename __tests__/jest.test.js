d4ef527d7c3bed73a94b7c203a8139aec619af1a
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-15 11:52:39 -0800

    add test for person object

--- __tests__/jest.test.js
+++ __tests__/jest.test.js
@@ -1,1 +1,8 @@
 
+describe("person object",() => {
+  test("should display person name and age in an object", () => {
+    let person1 = new Person("Ali", 27);
+    expect(person1.name).toEqual("Ali");
+    expect(person1.age).toEqual(27);
+  });
+});
