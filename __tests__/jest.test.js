050aa779fc4c038a3f8bfb17f3c35ae9c6e4c792
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-15 12:33:26 -0800

    add meaningfull fail test

--- __tests__/jest.test.js
+++ __tests__/jest.test.js
@@ -1,8 +1,8 @@
-
-describe("person object",() => {
+import {Person} from './../src/backend.js';
+describe("Person",() => {
   test("should display person name and age in an object", () => {
     let person1 = new Person("Ali", 27);
     expect(person1.name).toEqual("Ali");
-    expect(person1.age).toEqual(27);
+    expect(person1.age).toEqual(26);
   });
 });
