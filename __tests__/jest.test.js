c8997ea34658f8f2f0d8aea7654751c11c6e2414
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-15 13:09:54 -0800

    add second test to calculate ages on different planets

--- __tests__/jest.test.js
+++ __tests__/jest.test.js
@@ -1,8 +1,14 @@
-import {Person} from './../src/backend.js';
+import {Person, mercuryYear, venusYear, marsYear, jupiterYear} from './../src/backend.js';
 describe("Person",() => {
   test("should display person name and age in an object", () => {
-    let person1 = new Person("Ali", 27);
+    let person1 = new Person("Ali", 27, true, "earth");
     expect(person1.name).toEqual("Ali");
     expect(person1.age).toEqual(27);
+    expect(person1.exercise).toEqual(true)
+  });
+  test("calculate persons age on different planets", () => {
+    let person2 = new Person("Ali",27, true, "mercury");
+    expect(person2.checkAge()).toEqual(undefined);
+
   });
 });
--- src/backend.js
+++ src/backend.js
@@ -1,4 +1,11 @@
-export function Person(name, age){
+export const mercuryYear = .24
+export const venusYear = .62
+export const marsYear = 1.88
+export const jupiterYear = 11.68
+
+export function Person(name, age, exercise, planet){
   this.name = name;
   this.age = age;
+  this.exercise = true;
+  this.planet = planet;
 }
