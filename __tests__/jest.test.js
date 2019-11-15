658ef6f4b57cc0a5d2be2d045d2fa022dd8d76ab
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-15 14:54:54 -0800

    this will rewrite test to check how long they will live if they exercise or not

--- __tests__/jest.test.js
+++ __tests__/jest.test.js
@@ -1,17 +1,17 @@
 import {Person, mercuryYear, venusYear, marsYear, jupiterYear} from './../src/backend.js';
 describe("Person",() => {
-  test("should display person name and age in an object", () => {
-    let person1 = new Person("Ali", 27, true, "earth");
+  test("should display person name, age, yes or no if they exercise, and what planet in an object", () => {
+    let person1 = new Person("Ali", 27, "yes", "earth");
     expect(person1.name).toEqual("Ali");
     expect(person1.age).toEqual(27);
-    expect(person1.exercise).toEqual(true)
+    expect(person1.exercise).toEqual("yes")
   });
   test("calculate persons age on different planets", () => {
-    let person2 = new Person("Ali",27, true, "venus");
+    let person2 = new Person("Ali",27, "yes", "venus");
     expect(person2.checkAge()).toEqual(43);
   });
-  test("how many years they have left", () =>{
-    let person3 = new Person("Ali", 27, "mars");
-    expect(person3.yearsLeft()).toEqual(undefined);
+  test("how many years they will live if they anwer yes or no to exercising", () =>{
+    let person3 = new Person("Ali",27 ,"yes", "mercury");
+    expect(person3.checkExercise()).toEqual(99);
   });
 });
--- src/backend.js
+++ src/backend.js
@@ -2,14 +2,14 @@ export const mercuryYear = .24
 export const venusYear = .62
 export const marsYear = 1.88
 export const jupiterYear = 11.68
-export const averageHumanAge = 80
+
 
 
 export class Person {
   constructor (name, age, exercise, planet) {
   this.name = name;
   this.age = age;
-  this.exercise = true;
+  this.exercise = exercise;
   this.planet = planet;
   }
   checkAge(){
