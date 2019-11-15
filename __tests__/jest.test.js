9f9d3f5085cac7746bf8a55ac2db91a5f39247ab
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-15 15:44:59 -0800

    this will dry test code and add another test for life left on other planets

--- __tests__/jest.test.js
+++ __tests__/jest.test.js
@@ -1,17 +1,26 @@
 import {Person, mercuryYear, venusYear, marsYear, jupiterYear} from './../src/backend.js';
+
 describe("Person",() => {
+  let person1;
+  beforeEach(() => {
+    person1 = new Person("Ali", 27, "yes", "earth");
+  });
+
   test("should display person name, age, yes or no if they exercise, and what planet in an object", () => {
-    let person1 = new Person("Ali", 27, "yes", "earth");
     expect(person1.name).toEqual("Ali");
     expect(person1.age).toEqual(27);
     expect(person1.exercise).toEqual("yes")
   });
+
   test("calculate persons age on different planets", () => {
-    let person2 = new Person("Ali",27, "yes", "venus");
-    expect(person2.checkAge()).toEqual(43);
+    expect(person1.checkAge()).toEqual(43);
   });
+
   test("how many years they will live if they anwer yes or no to exercising", () =>{
-    let person3 = new Person("Ali",27 ,"yes", "mercury");
-    expect(person3.checkExercise()).toEqual(100);
+    expect(person1.checkExercise()).toEqual(100);
+  });
+
+  test("how many years they have left on a given planet", () =>{
+    expect(person1.checkPlanetYearsLeft()).toEqual(undefined);
   });
 });
