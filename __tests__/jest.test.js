1e6ab804a34be8f8132b643fb2f8bc5d08b3fed1
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-15 12:33:59 -0800

    pass Player object test

--- __tests__/jest.test.js
+++ __tests__/jest.test.js
@@ -3,6 +3,6 @@ describe("Person",() => {
   test("should display person name and age in an object", () => {
     let person1 = new Person("Ali", 27);
     expect(person1.name).toEqual("Ali");
-    expect(person1.age).toEqual(26);
+    expect(person1.age).toEqual(27);
   });
 });
